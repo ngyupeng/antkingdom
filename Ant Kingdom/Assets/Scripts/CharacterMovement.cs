211691a0fc6cf969a6931c544baf1c0ea8f45ebc
InvisibLemon <guanpeng2@gmail.com>
2023-05-12 20:52:40 +0800

    Added Simple Character Movement and Queen Ant Sprite

---
+++ Ant Kingdom/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Tilemap map;
+    [SerializeField] private float movementSpeed;
+    MouseInput mouseInput;
+    private Vector3 destination;
+    
+    private void Awake(){
+        mouseInput = new MouseInput();
+    }
+
+    private void OnEnable(){
+        mouseInput.Enable();
+    }
+
+    private void OnDisable(){
+        mouseInput.Disable();
+    }
+
+    void Start()
+    {
+        destination = transform.position;
+        mouseInput.Mouse.MouseClick.performed += _ => MouseClick();
+        Debug.Log("Mouse Click");
+    }
+
+
+    private void MouseClick(){
+        Vector2 mousePosition = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        if (map.HasTile(gridPosition)){
+            destination = mousePosition;
+        }  
+        Debug.Log("Mouse Click111");
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, destination) > 0.1f)
+        transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);  
+        //Debug.Log("Destination" + " " + destination);
+        //Debug.Log("transform.position" + " " + transform.position);
+    }
+}
